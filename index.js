8eaba20c574d0afd7887cafef2d24a65dfc67e85
rmenor_vertere <rmenor@vertere-gs.com>
2022-11-04 10:37:19 +0800

    Updated files for capstone project

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log('Hello World');
+// console.log('Hello World');
 
 AOS.init();
+
+
+let sendContactSuccessful = function(){
+	Swal.fire(
+	{
+		title: 'Message Sent!',
+		text: "",
+		icon: 'success',
+		confirmButtonColor: '#403d39ff'
+	}
+	)
+}
